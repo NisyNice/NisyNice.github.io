78af0000e1bb012e406db372e996004ae4cb4bcd
TyrrellSerrano <33523048+TyrrellSerrano@users.noreply.github.com>
2019-10-06 13:15:29 -0700

    v1

---
+++ main.js
@@ -0,0 +1,26 @@
+function menu(){
+    var x = document.getElementById('nav-links');
+    var y = document.getElementById('navBurger');
+    var z = document.getElementById('title');
+    var b = document.getElementsByTagName('body');
+    if (x.className === "nav-links") {
+            x.className += " responsive";
+            y.style.color="white";
+            z.style.color="white";
+           // window.addEventListener('scroll', noScroll);
+           b[0].style.overflow='hidden';
+           b[0].style.height='100%';
+    }
+    else {
+            x.className = "nav-links";
+            y.style.color="black";
+            z.style.color="black";
+           // window.removeEventListener('scroll', noScroll);
+           b[0].style.overflow='auto';
+           b[0].style.height='auto';
+    }
+}
+
+function noScroll(){
+    window.scrollTo(0,0);
+}
